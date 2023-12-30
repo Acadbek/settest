a7d3c1265df62ff3ae962bdc6288624b90cca910
Asadbek <ibnnumon@gmail.com>
2023-12-30 22:09:41 +0500

    ref: dashboard layout

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,9 +1,9 @@
 <template>
   <TheNav />
-  <main container mx-auto h-screen h-100dvh of-hidden>
-    <div class="grid grid-cols-4">
+  <main mx-auto h-screen h-100dvh of-hidden>
+    <div grid grid-cols-4 gap-4>
       <Sidebar col-span-1 />
-      <div class="border border-style col-span-3 h-100dvh of-hidden h-screen">
+      <div col-span-3 h-100dvh of-hidden h-screen>
         <slot />
       </div>
     </div>
