7c4c5cd3fb6fbcd4db86fe56c6b5c287a2fbe72a
Asadbek <ibnnumon@gmail.com>
2024-01-23 00:32:06 +0500

    chore: implement fetch cached data & add skeleton

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+const nuxtApp = useNuxtApp();
+
 definePageMeta({
   layout: "dashboard",
 });
@@ -17,17 +19,16 @@ const items = [
   },
 ];
 
-const { data: results } = useFetch("https://opentdb.com/api.php?amount=34&type=multiple");
-
-function methods(e) {
-  return { value: `a${e}`, label: `a${e}` };
-}
+const { data: results } = useFetch("https://opentdb.com/api.php?amount=3&type=multiple", {
+  getCachedData(key) {
+    return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
+  },
+});
 
 const selected = ref("sms");
 
 const mainForm = reactive({ name: results, username: "asas" });
 const mandatoryForm = reactive({ currentPassword: "asad", newPassword: "" });
-console.log(mandatoryForm, "pass");
 
 function onSubmit(form) {
   console.log("Submitted form:", form);
@@ -35,11 +36,11 @@ function onSubmit(form) {
 </script>
 
 <template>
-  <SlideOver :length="results?.results.length" />
+  <SlideOver v-if="results?.results" :length="results?.results.length" />
   <UTabs :items="items" class="w-full col-span-6">
     <template #item="{ item }">
       <UCard
-        @submit.prevent="() => onSubmit(item.key === 'asosiy' ? mainForm : mandatoryForm)"
+        @submit.prevent="() => onSubmit(item.key === 'asosiy' ? results : mandatoryForm)"
       >
         <template #header>
           <p class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
@@ -51,12 +52,20 @@ function onSubmit(form) {
         </template>
 
         <div v-if="item.key === 'asosiy'" class="space-y-3">
-          <div v-for="(question, i) in mainForm?.name.results" :key="question.question">
+          <div
+            v-if="results?.results"
+            v-for="(question, i) in results?.results"
+            :key="question.question"
+          >
             <div border border-base rounded-md p-4>
               <h3 :id="i + 1" class="text-[18px] pb-2">
                 {{ i + 1 }} - {{ question.question }}
               </h3>
             </div>
+          </div>
+          <div v-else v-for="_ in 5">
+            <USkeleton class="h-[50px] w-full mb-1" />
+            <USkeleton class="h-[50px] w-full" />
           </div>
         </div>
         <div v-else-if="item.key === 'majburiy'" class="space-y-3">
@@ -64,7 +73,7 @@ function onSubmit(form) {
         </div>
 
         <template #footer>
-          <UButton type="submit" color="primary" class="bg-primary">
+          <UButton :disabled="!results" type="submit" color="primary" class="bg-primary">
             Save {{ item.key === "asosiy" ? "asosiy" : "majburiy" }}
           </UButton>
         </template>
