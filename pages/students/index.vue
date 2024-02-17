14eff9db8d2637f2fb8ce040284c0be49cc76c4d
Asadbek <ibnnumon@gmail.com>
2024-02-17 23:32:27 +0500

    change main grid system

--- pages/students/index.vue
+++ pages/students/index.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
     <h1>Students</h1>
+    <div flex items-center gap-4>
+      <input type="checkbox" id="1" />
+      <label
+        class="text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:before:bg-gray-50 dark:hover:before:bg-gray-800/50 inline-block w-full"
+        for="1"
+        >uzb</label
+      >
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   layout: "dashboard",
-  middleware: ['auth-logged-in']
+  middleware: ["auth-logged-in"],
 });
 </script>
 
